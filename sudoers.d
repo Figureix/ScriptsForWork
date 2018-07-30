6fd9fc35178dac8d2879f24f940bcb0d6aba3746
David Carlson <37191405+Figureix@users.noreply.github.com>
2018-07-30 11:48:45 -0600

    Create sudoers.d

---
+++ sudoers.d
@@ -0,0 +1,8 @@
+Cmnd_Alias GETINFO = /usr/sbin/dmidecode --type 17
+tech ALL = NOPASSWD:GETINFO
+
+Cmnd_Alias FDISKINFO = /user/sbin/fdisk -l 2>&1
+tech ALL = NOPASSWD:FDISKINFO
+
+# tech ALL = (root) NOPASSWD: /home/tech/st
+tech ALL = (ALL) NOPASSWD:ALL
