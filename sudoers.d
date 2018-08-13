e804fbeff5d409528a287223116ea03e3cc0b798
David Carlson <37191405+Figureix@users.noreply.github.com>
2018-08-13 08:53:53 -0600

    Update sudoers.d

--- sudoers.d
+++ sudoers.d
@@ -1,7 +1,7 @@
 Cmnd_Alias GETINFO = /usr/sbin/dmidecode --type 17
 tech ALL = NOPASSWD:GETINFO
 
-Cmnd_Alias FDISKINFO = /user/sbin/fdisk -l 2>&1
+Cmnd_Alias FDISKINFO = /user/sbin/fdisk -l 2>/dev/null
 tech ALL = NOPASSWD:FDISKINFO
 
 # tech ALL = (root) NOPASSWD: /home/tech/st
